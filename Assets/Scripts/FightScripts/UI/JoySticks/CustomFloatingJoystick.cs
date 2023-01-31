2fb210043cc0ba0c59ed073c82ada1f0f6f73ebb
kadah.v (* ^ ω ^) <kadah.v@panoramikltd.com>
2023-02-01 01:19:52 +0300

    10 Hero choosing - added circular action menu and floating joystick

---
+++ Assets/Scripts/FightScripts/UI/JoySticks/CustomFloatingJoystick.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.OnScreen;
+
+namespace Editor.Scripts.FightScripts.UI.JoySticks
+{
+    public class CustomFloatingJoystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler 
+    {
+        [SerializeField] private FloatingOnScreenStick _joystick;
+
+        private void Awake()
+        {
+            _joystick.gameObject.SetActive(false);
+        }
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            _joystick.gameObject.SetActive(true);
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(transform.GetComponentInParent<RectTransform>(), eventData.position, eventData.pressEventCamera, out var position);
+            _joystick.OnPointerDown(eventData, position);
+        }
+        
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            _joystick.gameObject.SetActive(false);
+        }
+
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            _joystick.OnDrag(eventData);
+        }
+    }
+}
---
+++ Assets/Scripts/FightScripts/UI/JoySticks/FloatingOnScreenStick.cs
@@ -0,0 +1,83 @@
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+using UnityEngine.InputSystem.Layouts;
+
+namespace UnityEngine.InputSystem.OnScreen
+{
+    /// <summary>
+    /// A stick control displayed on screen and moved around by touch or other pointer
+    /// input.
+    /// </summary>
+    [AddComponentMenu("Input/FloatingOnScreenStick")]
+    //[HelpURL(InputSystem.kDocUrl + "/manual/OnScreen.html#on-screen-sticks")]
+    public class FloatingOnScreenStick : OnScreenControl, IPointerDownHandler, IPointerUpHandler, IDragHandler
+    {
+        
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (eventData == null)
+                throw new System.ArgumentNullException(nameof(eventData));
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(transform.parent.GetComponentInParent<RectTransform>(), eventData.position, eventData.pressEventCamera, out m_PointerDownPos);
+            //OnDrag(eventData);
+        }
+        
+        public void OnPointerDown(PointerEventData eventData, Vector2 screenPosition)
+        {
+            ((RectTransform) transform).anchoredPosition = screenPosition;
+            m_StartPos = screenPosition;
+            OnPointerDown(eventData);
+        }
+
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            if (eventData == null)
+                throw new System.ArgumentNullException(nameof(eventData));
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(transform.parent.GetComponentInParent<RectTransform>(), eventData.position, eventData.pressEventCamera, out var position);
+            var delta = position - m_PointerDownPos;
+
+            delta = Vector2.ClampMagnitude(delta, movementRange);
+            ((RectTransform)transform).anchoredPosition = m_StartPos + (Vector3)delta;
+
+            var newPos = new Vector2(delta.x / movementRange, delta.y / movementRange);
+            SendValueToControl(newPos);
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            gameObject.SetActive(false);
+            ((RectTransform)transform).anchoredPosition = m_StartPos;
+            SendValueToControl(Vector2.zero);
+        }
+
+        private void Start()
+        {
+            m_StartPos = ((RectTransform)transform).anchoredPosition;
+        }
+
+        public float movementRange
+        {
+            get => m_MovementRange;
+            set => m_MovementRange = value;
+        }
+
+        [FormerlySerializedAs("movementRange")]
+        [SerializeField]
+        private float m_MovementRange = 50;
+
+        [InputControl(layout = "Vector2")]
+        [SerializeField]
+        private string m_ControlPath;
+
+        private Vector3 m_StartPos;
+        private Vector2 m_PointerDownPos;
+
+        protected override string controlPathInternal
+        {
+            get => m_ControlPath;
+            set => m_ControlPath = value;
+        }
+    }
+}
