8cff8ae43d81335cfd84d434b3e4db64de9a61e2
kadah.v (* ^ ω ^) <kadah.v@panoramikltd.com>
2023-02-20 23:28:11 +0300

    11 bug fixing - added Manin menu localization

---
+++ Assets/Scripts/MainMenuScripts/UI/ChangeLocalizationUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+using UnityEngine.UI;
+
+namespace Editor.Scripts.MainMenuScripts.UI
+{
+	public class ChangeLocalizationUI : MonoBehaviour
+	{
+		private List<Locale> _locale;
+		private List<Locale> Locale => _locale ??= LocalizationSettings.AvailableLocales.Locales;
+		public void ChooseNextLanguage()
+		{
+			int index = Locale.IndexOf(LocalizationSettings.SelectedLocale);
+			index++;
+			if (index >= Locale.Count)
+			{
+				index = 0;
+			}
+			LocalizationSettings.SelectedLocale = Locale[index];
+		}
+	}
+}
