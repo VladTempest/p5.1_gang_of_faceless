b5c07c6bafc529d307792feadf98897ae9db6167
kadah.v (* ^ ω ^) <kadah.v@panoramikltd.com>
2023-03-02 02:32:33 +0300

    11 bug fixing localization - finalizes localization for Action button description

--- Assets/Scripts/FightScripts/UI/ActionUI/ActionButtonUI.cs
+++ Assets/Scripts/FightScripts/UI/ActionUI/ActionButtonUI.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using Editor.Scripts.GlobalUtils;
 using Editor.Scripts.Utils;
+using Sirenix.OdinInspector;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -18,13 +20,47 @@ public class ActionButtonUI : MonoBehaviour, IPointerEnterHandler, IPointerExitH
     [SerializeField] private LocalizeStringEvent _actionNameStringEvent;
     [SerializeField] private Button _button;
     [SerializeField] private GameObject _selectedVisual;
+
+    [Header("String References")]
     [SerializeField] private LocalizeStringEvent _chargesLeft;
     private LocalizedString _chargesLeftStringrReference;
     private TextMeshProUGUI _chargesLeftText;
     
+    [SerializeField] private LocalizeStringEvent _damageString;
+    private LocalizedString _damageStringReference;
+    private TextMeshProUGUI _damageStringText;
+    
+    [SerializeField] private LocalizeStringEvent _rangeString;
+    private LocalizedString _rangeStringReference;
+    private TextMeshProUGUI _rangeStringText;
+
+    [SerializeField] private LocalizeStringEvent _coolDownString;
+    private LocalizedString _coolDownStringReference;
+    private TextMeshProUGUI _coolDownStringText;
+    
+    [SerializeField] private LocalizeStringEvent _actionCostString;
+    private LocalizedString _actionCostStringReference;
+    private TextMeshProUGUI _actionCostStringText;
+    
+    [SerializeField] private LocalizeStringEvent _chargesString;
+    private LocalizedString _chargesStringReference;
+    private TextMeshProUGUI _chargesStringText;
+    
+    [SerializeField] private LocalizeStringEvent _nameString;
+    private LocalizedString _nameStringReference;
+    private TextMeshProUGUI _nameStringText;
+
+    [SerializeField] private LocalizeStringEvent _descriptionString;
+    private LocalizedString _descriptionStringReference;
+    private TextMeshProUGUI _descriptionStringText;
+    
+    private Dictionary<LocalizeStringEvent, Tuple<LocalizedString, TextMeshProUGUI>> _localizeStringEventsDictionary;
+    
+    [Header("Action Description UI")]
     [FormerlySerializedAs("_apActionDescription")] [FormerlySerializedAs("_apDamageRange")] [SerializeField] private TextMeshProUGUI _actionDescription;
-    [SerializeField] private Image _coolDownMask;
     [SerializeField] private GameObject _actionDescriptionUI;
+    
+    [SerializeField] private Image _coolDownMask;
 
     private BaseAction _baseAction;
     
@@ -35,6 +71,18 @@ public class ActionButtonUI : MonoBehaviour, IPointerEnterHandler, IPointerExitH
     private void Awake()
     {
         _button = GetComponent<Button>();
+        
+        _localizeStringEventsDictionary = new Dictionary<LocalizeStringEvent, Tuple<LocalizedString, TextMeshProUGUI>>
+        {
+            {_chargesLeft, new Tuple<LocalizedString, TextMeshProUGUI>(_chargesLeftStringrReference = _chargesLeft.StringReference, _chargesLeftText = _chargesLeft.GetComponent<TextMeshProUGUI>())},
+            {_damageString, new Tuple<LocalizedString, TextMeshProUGUI>(_damageStringReference = _damageString.StringReference, _damageStringText = _damageString.GetComponent<TextMeshProUGUI>())},
+            {_rangeString, new Tuple<LocalizedString, TextMeshProUGUI>(_rangeStringReference = _rangeString.StringReference, _rangeStringText = _rangeString.GetComponent<TextMeshProUGUI>())},
+            {_coolDownString, new Tuple<LocalizedString, TextMeshProUGUI>(_coolDownStringReference = _coolDownString.StringReference, _coolDownStringText = _coolDownString.GetComponent<TextMeshProUGUI>())},
+            {_actionCostString, new Tuple<LocalizedString, TextMeshProUGUI>(_actionCostStringReference = _actionCostString.StringReference, _actionCostStringText = _actionCostString.GetComponent<TextMeshProUGUI>())},
+            {_chargesString, new Tuple<LocalizedString, TextMeshProUGUI>(_chargesStringReference = _chargesString.StringReference, _chargesStringText = _chargesString.GetComponent<TextMeshProUGUI>())},
+            {_nameString, new Tuple<LocalizedString, TextMeshProUGUI>(_nameStringReference = _nameString.StringReference, _nameStringText = _nameString.GetComponent<TextMeshProUGUI>())},
+            {_descriptionString, new Tuple<LocalizedString, TextMeshProUGUI>(_descriptionStringReference = _descriptionString.StringReference, _descriptionStringText = _descriptionString.GetComponent<TextMeshProUGUI>())},
+        };
     }
 
     private void BaseAction_OnActionStatusUpdate(object sender, EventArgs e)
@@ -44,32 +92,27 @@ public class ActionButtonUI : MonoBehaviour, IPointerEnterHandler, IPointerExitH
         UpdateButtonInteractivity();
     }
 
+    
     public void SetBaseAction(BaseAction baseAction)
     {
-        SetUpLocalizesStringAccordinglyToType(baseAction);
-
         _baseAction = baseAction;
-        if (!(_baseAction is MoveAction))
-        {
-            var damageString = _baseAction.Damage == 0 ? "" : $"It deals  <b><i>{_baseAction.Damage}</i></b> damage."+"\n";
-            var actionCostString =_baseAction.GetActionPointCost() ==0 ? "" : $"Action costs  <b><i>{_baseAction.GetActionPointCost()}</i></b> points" +"\n";
-            var rangeString = _baseAction.MaxActionRange <= 1 ? "" : $"It has range from  <b><i>{_baseAction.MinActionRange}</i></b> to  <b><i>{_baseAction.MaxActionRange}</i></b> cells." +"\n";
-            var cooldownString = !_baseAction.HasCoolDown ? "" : $"Time to reload is  <b><i>{_baseAction.FullCoolDownValue}</i></b> turns." +"\n";
-            var chargesString = !_baseAction.IsChargeable ? "" : $"Action has  <b><i>{_baseAction.MaxCharges}</i></b> charges." +"\n";
-            
-            _actionDescription.text = $"<b>{_baseAction.GetActionName()}</b>" + "\n"+ $"{_baseAction.Description}"+"\n"+"\n"+
-                                        actionCostString+
-                                        damageString+
-                                        rangeString+
-                                        cooldownString+
-                                        chargesString;
-        }
-        else
-        {
-            _actionDescription.text =  $"<b>{_baseAction.GetActionName()}</b>" + "\n"+ $"{_baseAction.Description}";
-        }
 
-        _button.onClick.AddListener(() => UnitActionSystem.Instance.SetSelectedAction(baseAction));
+        SetUpLocalizesStringAccordinglyToType(_actionNameStringEvent, _baseAction.GetActionName());
+        SetUpLocalizesStringAccordinglyToType(_nameString, _baseAction.GetActionName());
+        SetUpLocalizesStringAccordinglyToType(_descriptionString, _baseAction.Description);
+
+        
+            InitialSetUpLocalizedString(_damageString, _baseAction.Damage);
+            if (_baseAction is not MoveAction) InitialSetUpLocalizedString(_actionCostString, _baseAction.GetActionPointCost());
+            InitialSetUpLocalizedString(_rangeString, new object[] {_baseAction.MinActionRange, baseAction.MaxActionRange});
+            InitialSetUpLocalizedString(_coolDownString, _baseAction.FullCoolDownValue);
+            InitialSetUpLocalizedString(_chargesString, _baseAction.MaxCharges);
+        
+
+        CreateDescription();
+
+        if (LocalizationSettings.Instance != null) LocalizationSettings.Instance.OnSelectedLocaleChanged += OnSelectedLocaleChanged;
+        _button.onClick.AddListener(() => UnitActionSystem.Instance.SetSelectedAction(_baseAction));
         SetUpActionVisuals();
         _baseAction.OnActionStatusUpdate += BaseAction_OnActionStatusUpdate;
         UnitActionSystem.Instance.OnSelectedActionChanged += BaseAction_OnActionStarted;
@@ -78,11 +121,52 @@ public class ActionButtonUI : MonoBehaviour, IPointerEnterHandler, IPointerExitH
 
     }
 
-    private void SetUpLocalizesStringAccordinglyToType(BaseAction baseAction)
+    private void CreateDescription()
     {
-        var stringRef = _actionNameStringEvent.StringReference;
+        if (_baseAction is not MoveAction)
+        {
+            var damageString = _baseAction.Damage == 0
+                ? ""
+                : $"{_damageString.StringReference.GetLocalizedString()}" + "\n";
+            var actionCostString = _baseAction.GetActionPointCost() == 0
+                ? ""
+                : $"{_actionCostString.StringReference.GetLocalizedString()}" + "\n";
+            var rangeString = _baseAction.MaxActionRange <= 1
+                ? ""
+                : $"{_rangeString.StringReference.GetLocalizedString()}" +
+                  "\n";
+            var cooldownString = !_baseAction.HasCoolDown
+                ? ""
+                : $"{_coolDownString.StringReference.GetLocalizedString()}" + "\n";
+            var chargesString = !_baseAction.IsChargeable
+                ? ""
+                : $"{_chargesString.StringReference.GetLocalizedString()}" + "\n";
+
+            _actionDescription.text = $"<b>{_nameString.StringReference.GetLocalizedString()}</b>" + "\n" +
+                                      $"{_descriptionString.StringReference.GetLocalizedString()}" + "\n" + "\n" +
+                                      actionCostString +
+                                      damageString +
+                                      rangeString +
+                                      cooldownString +
+                                      chargesString;
+        }
+        else
+        {
+            _actionDescription.text = $"<b>{_nameString.StringReference.GetLocalizedString()}</b>" + "\n" +
+                                      $"{_descriptionString.StringReference.GetLocalizedString()}";
+        }
+    }
+
+    private void OnSelectedLocaleChanged(Locale obj)
+    {
+        CreateDescription();
+    }
+
+    private void SetUpLocalizesStringAccordinglyToType(LocalizeStringEvent actionStringEvent, TableEntryReference tableEntryReference)
+    {
+        var stringRef = actionStringEvent.StringReference;
         stringRef.TableReference = "ActionUI";
-        stringRef.TableEntryReference = baseAction.GetActionName();
+        stringRef.TableEntryReference = tableEntryReference;
         stringRef.GetLocalizedString();
     }
 
@@ -109,7 +193,7 @@ public class ActionButtonUI : MonoBehaviour, IPointerEnterHandler, IPointerExitH
         if (_baseAction.IsChargeable)
         {
             _chargesLeft.gameObject.SetActive(true);
-            InitialSetUpChargesString();
+            InitialSetUpLocalizedString(_chargesLeft, OnChargesLeftStringrReferenceOnStringChanged, _baseAction.ChargesLeft);
             UpdateChargesVisuals();
         }
 
@@ -144,27 +228,34 @@ public class ActionButtonUI : MonoBehaviour, IPointerEnterHandler, IPointerExitH
         _chargesLeftStringrReference.RefreshString();
     }
 
-    private bool InitialSetUpChargesString()
+    private void InitialSetUpLocalizedString(LocalizeStringEvent stringEvent, LocalizedString.ChangeHandler stringChangedCallback, params object[] dynamicStringArguments)
     {
-        _chargesLeftStringrReference = _chargesLeft.StringReference;
-        _chargesLeftText = _chargesLeft.GetComponent<TextMeshProUGUI>();
-
-
-        if (_chargesLeftText == null) return true;
-        _chargesLeftText.text = _baseAction.ChargesLeft.ToString();
-
-        if (_chargesLeftStringrReference.Arguments == null)
+        if (_localizeStringEventsDictionary[stringEvent].Item2 == null) return;
+        
+        InitialSetUpLocalizedString(stringEvent, dynamicStringArguments);
+        
+        _localizeStringEventsDictionary[stringEvent].Item1.StringChanged += stringChangedCallback;
+    }
+    
+    private void InitialSetUpLocalizedString(LocalizeStringEvent stringEvent, params object[] dynamicStringArguments)
+    {
+        if (_localizeStringEventsDictionary[stringEvent].Item1.Arguments == null)
         {
-            _chargesLeftStringrReference.Arguments = new object[] {_baseAction.ChargesLeft};
-            _chargesLeftStringrReference.StringChanged += OnChargesLeftStringrReferenceOnStringChanged;
+            _localizeStringEventsDictionary[stringEvent].Item1.Arguments =  dynamicStringArguments;
         }
 
-        return false;
+        for (int i = 0; i < _localizeStringEventsDictionary[stringEvent].Item1.Arguments.Count; i++)
+        {
+            _localizeStringEventsDictionary[stringEvent].Item1.Arguments[i] = dynamicStringArguments[i];
+            ConvenientLogger.Log(nameof(ActionButtonUI), GlobalLogConstant.IsActionLogEnabled, $"{_baseAction.GetActionName()} {stringEvent.StringReference.GetLocalizedString()} : " + _localizeStringEventsDictionary[stringEvent].Item1.Arguments[i].ToString());
+        }
+
+        stringEvent.RefreshString();
     }
 
     private void OnChargesLeftStringrReferenceOnStringChanged(string value)
     {
-        _chargesLeftText.text = value;
+        _localizeStringEventsDictionary[_chargesLeft].Item2.text = value;
     }
 
     private void OnDestroy()
@@ -174,6 +265,7 @@ public class ActionButtonUI : MonoBehaviour, IPointerEnterHandler, IPointerExitH
         UnitActionSystem.Instance.OnSelectedUnitChanged -= BaseAction_OnActionStarted;
         TurnSystem.Instance.OnTurnChanged -= BaseAction_OnActionStarted;
         if (_chargesLeftStringrReference != null) _chargesLeftStringrReference.StringChanged -= OnChargesLeftStringrReferenceOnStringChanged;
+        if (LocalizationSettings.Instance != null) LocalizationSettings.Instance.OnSelectedLocaleChanged -= OnSelectedLocaleChanged;
     }
 
     private void ShowActionDescriptionUI()
