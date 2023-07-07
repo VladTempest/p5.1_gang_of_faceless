0aff1f4fe3600f48bc8ff597e21c27e54346adb1
kadah.v (* ^ ω ^) <kadah.v@panoramikltd.com>
2023-07-07 10:11:00 +0300

    13 Hub Location - Added resource craft properties and related scriptable object
    Added two new files for managing different properties of craftable resources in the Hub Location. The first file, ResourceCraftPropertySO.cs, contains a scriptable object that acts as a dictionary of all craftable resources along with their properties inside the game. The OnValidate method ensures the proper assigning of resource names and types. The second file, ResourceCraftProperty.cs, creates a simple structure that holds the name, type, and cost of a craftable resource. These changes are needed in order to manage and store the various properties related to each craftable resource.

---
+++ Assets/Scripts/HubLocation/ResourceManagement/ResourcesSO/ResourceCraftProperty.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Editor.Scripts.HubLocation.ResourcesSO
+{
+	[Serializable]
+	public struct ResourceCraftProperty
+	{
+		[HideInInspector]
+		public string Name;
+		[HideInInspector]
+		public ResourceTypes ResourceType;
+		public SerializableDictionary<ResourceTypes, int> Cost;
+
+		public ResourceCraftProperty(string name, ResourceTypes resourceType, SerializableDictionary<ResourceTypes, int> cost)
+		{
+			this.Name = name;
+			this.Cost = cost;
+			this.ResourceType = resourceType;
+		}
+	}
+}
---
+++ Assets/Scripts/HubLocation/ResourceManagement/ResourcesSO/ResourceCraftPropertySO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Editor.Scripts.HubLocation.ResourcesSO
+{
+	[CreateAssetMenu(fileName = "ResourceCraftPropertySO", menuName = "ScriptableObjects/ResourceCraftPropertySO", order = 1)]
+	public class ResourceCraftPropertySO : ScriptableObject
+	{
+		public SerializableDictionary<ResourceTypes, ResourceCraftProperty> ResourceCraftPropertyDictionary;
+
+		private void OnValidate()
+		{
+			//insert in name field nameof(ResourceType) Key
+			var list = ResourceCraftPropertyDictionary.ToList();
+			for (int i = 0; i < list.Count; i++)
+			{
+				var keyValuePair = list[i];
+				var craftProperty = keyValuePair.Value;
+				craftProperty.Name = keyValuePair.Key.ToString();
+				craftProperty.ResourceType = keyValuePair.Key;
+				ResourceCraftPropertyDictionary[keyValuePair.Key] = craftProperty;
+			}
+		}
+	}
+}
